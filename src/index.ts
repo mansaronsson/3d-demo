41203cdb298600ce19eaa82269deb4b53126e074
Måns Aronsson <mans@orbital-systems.com>
2024-10-27 11:19:57 +0100

    asset: Add and load models
    Table and lamps from two .glb files.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 // Scene
 const scene = new THREE.Scene();
@@ -10,6 +11,32 @@ camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+// Load models
+let table_model: THREE.Group;
+const loader = new GLTFLoader();
+loader.load(
+    'assets/models/table.glb',
+    (gltf) => {
+        table_model = gltf.scene;
+        table_model.position.set(-.5, 0, -.5);
+        scene.add(table_model);
+    },
+    undefined,
+    (error) => { console.error(error); }
+);
+let lamps_model: THREE.Group;
+loader.load(
+    'assets/models/lamp.glb',
+    (gltf) => {
+        lamps_model = gltf.scene;
+        lamps_model.scale.set(0.1, 0.1, 0.1);
+        lamps_model.position.set(0.5, -0.2, .5);
+        scene.add(lamps_model);
+    },
+    undefined,
+    (error) => { console.error(error); }
+);
 
 // Animation loop
 function animate() {
